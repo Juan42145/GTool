7adeb58c8b10bf34e89a0aa9fbd0e816ddf607f5
Juan42145 <juancamilo.arias@hotmail.com>
2022-08-02 18:29:45 -0500

    Refactor server

--- main/server.js
+++ main/server.js
@@ -1,104 +1,74 @@
 //const url = 'https://script.google.com/macros/s/AKfycbyV8Y3q9Qm7xbFs_6fMPgX4_Sm-T4Nr37jJgAiPKl_N/dev';
 const url = 'https://script.google.com/macros/s/AKfycbwCioh-jh0XCMIi_BwhsIybNf1JWA4hfZQkdfBsFWjjnbLiMSbqNigpG4zTEvqzLPOUhw/exec';
 
-/*GETTERS*/
+function server(query){
+  const script = document.createElement('script'); document.body.append(script);
+  script.setAttribute('src', url + '?' + query); 
+}
+
+/*--GETTERS--*/
 function getDB(){
-  const script = document.createElement('script');
-  script.setAttribute('src', url + '?getDB=handleDB');
-  document.body.append(script);
+  server('getDB=handleDB');
 }
 
 function getUser(){
-  const script = document.createElement('script');
-  script.setAttribute('src', url + '?getUser=handleUser');
-  document.body.append(script);
+  server('getUser=handleUser');
 }
 
 function getAuth(p){
-  const script = document.createElement('script');
-  script.setAttribute('src', url + `?a=handleAuth&p=${p}`);
-  document.body.append(script);
+  server('a=handleAuth&p='+p);
 }
 
-/*SETTERS*/
-function setInv(){
-  let store = JSON.stringify(sessionStorage.get('cacheI'));
-  console.log(store)
+/*--SETTERS--*/
+function setter(string, query){
+  let store = JSON.stringify(sessionStorage.get('cache'+string[0]));
+  console.log(store);
   if(store == 'null'){
-    toast('No Inventory to Save');
-    return;
+    toast(`No ${string} to Save`); return;
   }
+  toast('Saving '+string); server(query+'&cord='+store);
+}
 
-  toast('Saving Inventory');
-  const script = document.createElement('script');
-  script.setAttribute('src', url + `?setInv=handleInv&cord=${store}`);
-  document.body.append(script);
+function setInv(){
+  setter('Inventory','setInv=handleInv');
 }
 
 function setChar(){
-  let store = JSON.stringify(sessionStorage.get('cacheC'));
-  console.log(store)
-  if(store == 'null'){
-    toast('No Character to Save');
-    return;
-  }
-  
-  toast('Saving Characters');
-  const script = document.createElement('script');
-  script.setAttribute('src', url + `?setChar=handleChar&cord=${store}`);
-  document.body.append(script);
+  setter('Characters','setChar=handleChar');
 }
 
 function setWpn(){
-  let store = JSON.stringify(sessionStorage.get('cacheW'));
-  console.log(store)
-  if(store == 'null'){
-    toast('No Weapon to Save');
-    return;
-  }
-  
-  toast('Saving Weapons');
-  const script = document.createElement('script');
-  script.setAttribute('src', url + `?setWpn=handleWpn&cord=${store}`);
-  document.body.append(script);
+  setter('Weapons','setWpn=handleWpn');
 }
 
-/*HANDLERS*/
+/*--HANDLERS--*/
 function handleUser(data){
-  console.log(data);
-  receiveUser(data);
+  console.log(data); receiveUser(data);
 }
 
 function handleDB(data){
-  console.log(data);
-  receiveDB(data);
+  console.log(data); receiveDB(data);
 }
 
 function handleAuth(isCorrect){
-  console.log(isCorrect);
-  receiveAuth(isCorrect);
+  console.log(isCorrect); receiveAuth(isCorrect);
 }
 
 function handleInv(){
-  toast('Saved Inventory');
-  sessionStorage.set('cacheI',null);
+  toast('Saved Inventory'); sessionStorage.set('cacheI',null);
 }
  
 function handleChar(){
-  toast('Saved Characters');
-  sessionStorage.set('cacheC',null);
+  toast('Saved Characters'); sessionStorage.set('cacheC',null);
 }
 
 function handleWpn(){
-  toast('Saved Weapons');
-  sessionStorage.set('cacheW',null);
+  toast('Saved Weapons'); sessionStorage.set('cacheW',null);
 }
 
-/*ALERT*/
+/*--ALERT--*/
 function toast(message){
-  const TOAST = document.createElement('div');
-  TOAST.classList = 'alert';
-  TOAST.textContent = message;
-  document.body.append(TOAST);
+  const TOAST = document.createElement('div'); document.body.append(TOAST);
+  TOAST.classList = 'alert'; TOAST.textContent = message;
   setTimeout(()=>TOAST.remove(),1500)
 }
