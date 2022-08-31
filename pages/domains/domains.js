28e4550f0288d544b7a5c0faf9d4ebdea5934a35
Juan42145 <juancamilo.arias@hotmail.com>
2022-08-30 20:31:32 -0500

    Partial work on domains

--- pages/domains/domains.js
+++ pages/domains/domains.js
@@ -1,85 +1,66 @@
 const LDB = sessionStorage.get('DB');
 let userChar = sessionStorage.get('user').Characters;
+let userWpn = sessionStorage.get('user').Weapons;
 let isShown = false; isLine = false;
 
 function domains(){
-  document.getElementById('c-cols').innerHTML = '';
-  document.getElementById('c-rows').innerHTML = '';
-  document.getElementById('c-table').innerHTML = '';
+  makeTable(true);
+  makeTable(false);
+}
+
+function makeTable(isChar){
+  let id = isChar? 'c-': 'w-';
+  document.getElementById(id+'rows').innerHTML = '';
+  document.getElementById(id+'table').innerHTML = '';
 
   let isOwned = document.getElementById('owned').checked;
-  let rows = getHeaders();
+  let rows = getHeaders(isChar);
 
   let nRows = rows.length, nCols = 1;
-  const TABLE = document.getElementById('c-table');
+  const TABLE = document.getElementById(id+'table');
   let cells = new Array(nRows);
   for(let r = 0; r <= nRows; r++){
     cells[r] = new Array(nCols);
     for(let c = 0; c <= nCols; c++){
       const DIV = create(TABLE, 'div', {'class':'cell'})
-      DIV.style = `grid-column: ${c+3}; grid-row: ${r+3};`
+      DIV.style = `grid-column: ${c+3}; grid-row: ${r+1};`
       cells[r][c] = DIV;
     }
   }
   let totals = [new Array(nRows).fill(0), new Array(nCols).fill(0)]
-  getChar(cells, rows, isOwned, totals);
+  if(isChar) getChar(cells, rows, isOwned, totals);
+  else getWpn(cells, rows, isOwned, totals);
   makeTotals(cells, nRows, nCols, totals);
 }
 
-function translate(category){
-  if(category === "BOSS") return "BOSSES";
-  else if(category === "WEEKLY") return "WEEKLYS";
-  else if(category === "BOOK") return "BOOKS";
-  else if(category === "LOCAL") return "LOCALS";
-  else return category;
-}
+function getHeaders(isChar){
+  let array = 0, rank = 0;
+  let id = isChar? 'c-': 'w-', name = isChar? 'BOOKS': 'TROPHIES';
 
-function getHeaders(){
-  let array = 0, rank = 0, isText = false, span = 3, cum = 3, prev = 3;
-  let isSet = true;
-  isLine = true
+  array = Object.keys(LDB.DB_Master[name]);
+  rank = Object.keys(LDB.DB_Master[name][array[0]])[0];
 
-  //CHAR AND WEAPON
-  array = Object.keys(LDB.DB_Master['BOOKS']);
-  rank = Object.keys(LDB.DB_Master['BOOKS'][array[0]])[0];
-  
-
-  let HEAD;
-  HEAD = document.getElementById('c-rows');
-  //else HEAD = document.getElementById('c-cols');
+  let HEAD = document.getElementById(id+'rows');
   array.forEach(item => {
-    const CARD = create(HEAD, 'div', {'class':'row'+' header'})
-
-    const IMG = create(CARD, 'img', {'class':'image','src':getImage('BOOKS', item, rank)})
+    const CARD = create(HEAD, 'div', {'class':'row header'})
+    const IMG = create(CARD, 'img', {'class':'image','src':getImage(name, item, rank)})
     setError(IMG)
     CARD.addEventListener('mouseover', ()=>tooltip.show(item))
     CARD.addEventListener('mouseout', ()=>tooltip.hide())
-    
   });
 
-    let group = { 'LOCALS': 'REGION', 'BOOKS': 'REGION', 'WEEKLYS': 'WEEKLY BOSS'};
-    Object.keys(LDB.DB_Master[group['BOOKS']]).forEach((item, i) => {
-      if(Object.keys(LDB.DB_Master['BOOKS']).length/3 <= i) return;
-
-      const CARD = create(HEAD, 'div', {'class':'row'+'-group header'})
-
-      let cStyle = 'grid-row'
-      CARD.style = cStyle+': span '+span;
-
-      const IMG = create(CARD, 'img', {'class':'image','src':getImage(group['BOOKS'], item, 0)})
+    Object.keys(LDB.DB_Master['REGION']).forEach((item, i) => {
+      if(Object.keys(LDB.DB_Master[name]).length/3 <= i) return;
+      const CARD = create(HEAD, 'div', {'class':'row-group header'})
+      const IMG = create(CARD, 'img', {'class':'image','src':getImage('REGION', item, 0)})
       setError(IMG)
     });
-    HEAD.classList.add('group')
-    document.getElementById('container').classList.add('rowG')
 
-
-  const CARD = create(HEAD, 'div', {'class':'row'+' header htotal'})
-
+  const CARD = create(HEAD, 'div', {'class':'row header htotal'})
   return array;
 }
 
 function getChar(array, rHeaders, check, totals){
-  //(cells, rowi, coli, rows, cols, isOwned, totals);
   let data = LDB.DB_Characters;
   Object.entries(data).forEach(([name, info]) => {
     if(check) if(!userChar[name].OWNED) return;
@@ -89,8 +70,27 @@ function getChar(array, rHeaders, check, totals){
     const CARD = create(array[rowi][coli], 'div', {'class':'card'})
     totals[0][rowi]++; totals[1][coli]++;
     
-    const IMG = create(CARD, 'img', {'class':'char-image c_'+info.RARITY,
+    const IMG = create(CARD, 'img', {'class':'icon c_'+info.RARITY,
       'src':getCharacter(name)})
+    setError(IMG)
+  });
+}
+
+function getWpn(array, rHeaders, check, totals){
+  let data = LDB.DB_Weapons;
+  Object.entries(data).forEach(([name, info]) => {
+    if(check) if(!userWpn[name].OWNED) return;
+    let rowi = rHeaders.indexOf(info['TROPHY']), coli = 0
+
+    if(rowi === -1 || coli === -1) return;
+    const CARD = create(array[rowi][coli], 'div', {'class':'card'})
+    totals[0][rowi]++; totals[1][coli]++;
+
+    CARD.addEventListener('mouseover', ()=>tooltip.show(name))
+    CARD.addEventListener('mouseout', ()=>tooltip.hide())
+    
+    const IMG = create(CARD, 'img', {'class':'icon c_'+info.RARITY,
+      'src':getWeapon(name)})
     setError(IMG)
   });
 }
