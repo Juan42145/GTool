4d6eb68734f348982a6f55559672b449d82d44c4
Juan42145 <juancamilo.arias@hotmail.com>
2023-08-08 16:21:26 -0500

    Update data table

--- pages/data/data.js
+++ pages/data/data.js
@@ -25,14 +25,19 @@ function makeTable(name, data){
   Object.entries(data).forEach(([category, rows], ci)=>{
     let colH = Object.keys(Object.values(rows)[0])
     let cClass = ci%2?'odd':'even';
+    let total = colH.length > 1;
 
-    let cat = create(hCatg, 'th', {'colspan':colH.length, 'class':cClass})
+    let cat = create(hCatg, 'th', {'colspan':colH.length + total, 'class':cClass})
     cat.textContent = category
 
     colH.forEach(r => {
       let rank = create(hRank, 'th', {'class':cClass})
       rank.textContent = r
     })
+    if(total){
+      let rank = create(hRank, 'th', {'class':cClass})
+      rank.textContent = 'Total'
+    }
 
     Object.entries(rows).forEach(([row, cols], ri)=>{
       if(!rowArray[row]){
@@ -42,12 +47,17 @@ function makeTable(name, data){
       }
       if(ri === 0) f = row;
       let prev = (rows[row-1] && !isCum)? Object.values(rows[row-1]): []
+      let tvalue = 0
+      let max = Object.values(cols).length-1
       Object.values(cols).forEach((cumm, i)=>{
         let cell = create(rowArray[row],'td',{'class':cClass})
         let value = (row === f || isCum)? cumm: cumm - +prev[i]
         if(value === 0) value = '';
         cell.textContent = value
+        tvalue += +value / (3**(max-i))
       })
+      let tcell = total? create(rowArray[row],'td',{'class':cClass+' total'}): ''
+      tcell.textContent = (Math.floor(tvalue*100)/100).toLocaleString('en-us')
     })
   })
 }
